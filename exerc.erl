05172865722d6605be1aa35755b513095b5dca0b
Pontus Stjernström <Pontus Stjernstrm>
2016-09-27 10:52:47 +0200

    No comment.

--- exerc.erl
+++ exerc.erl
@@ -10,3 +10,19 @@ reverseHelp([], List) -> List;
 reverseHelp([X|XS], List) ->
     reverseHelp(XS, [X] ++ List).
     
+find(_, []) -> not_found;
+find(Elem, [X|XS]) -> 
+    if
+        X == Elem -> {found, Elem};
+        true      -> find(Elem, XS)
+    end.
+    
+delete(Elem, List) ->
+    deleteHelp(Elem, List, []).
+    
+deleteHelp(_, [], List) -> List;
+deleteHelp(Elem, [X|XS], List) ->
+    if
+        X == Elem -> List ++ XS;
+        true -> deleteHelp(Elem, XS, List ++ [X])
+    end.
